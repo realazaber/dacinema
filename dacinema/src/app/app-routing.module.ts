b843a862d712f13ccdb71553e225f03615e07b3f
swagoverlord <azcooldude8@gmail.com>
2023-04-04 13:48:28 +0200

    base universal components

--- dacinema/src/app/app-routing.module.ts
+++ dacinema/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }, { path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) }, { path: 'movies', loadChildren: () => import('./pages/movies/movies.module').then(m => m.MoviesModule) }, { path: 'mymovies', loadChildren: () => import('./pages/mymovies/mymovies.module').then(m => m.MymoviesModule) }, { path: 'contact', loadChildren: () => import('./pages/contact/contact.module').then(m => m.ContactModule) }];
+const routes: Routes = [
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) },
+  { path: 'movies', loadChildren: () => import('./pages/movies/movies.module').then(m => m.MoviesModule) },
+  { path: 'mymovies', loadChildren: () => import('./pages/mymovies/mymovies.module').then(m => m.MymoviesModule) },
+  { path: 'contact', loadChildren: () => import('./pages/contact/contact.module').then(m => m.ContactModule) }];
+
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
