dd2207895e045f78a5b6949e4386dc4ff7526fad
swagoverlord <azcooldude8@gmail.com>
2023-04-06 18:55:32 +0200

    Created backend for the api

---
+++ backend/app/Http/Controllers/MoviesController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class MoviesController extends Controller
+{
+    public function index()
+    {
+
+        $response = Http::withHeaders([
+            'X-RapidAPI-Key' => env('API_KEY'),
+            'X-RapidAPI-Host' => 'ott-details.p.rapidapi.com'
+        ])->get('https://ott-details.p.rapidapi.com/advancedsearch', [
+            'language' => 'english',
+            'type' => 'movie',
+            'sort' => 'latest',
+            'page' => '1'
+        ]);
+
+        return response()->json($response->json());
+    }
+
+    public function single($imdbid)
+    {
+        $response = Http::withHeaders([
+            'X-RapidAPI-Key' => env('API_KEY'),
+            'X-RapidAPI-Host' => 'ott-details.p.rapidapi.com'
+        ])->get('https://ott-details.p.rapidapi.com/gettitleDetails', [
+            'imdbid' => $imdbid,
+            'page' => '1'
+        ]);
+
+        return response()->json($response->json());
+    }
+
+    public function search($search)
+    {
+        $response = Http::withHeaders([
+            'X-RapidAPI-Key' => env('API_KEY'),
+            'X-RapidAPI-Host' => 'ott-details.p.rapidapi.com'
+        ])->get('https://ott-details.p.rapidapi.com/search', [
+            'title' => $search,
+            'page' => '1'
+        ]);
+
+        return response()->json($response->json());
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\MoviesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/movies', [MoviesController::class, 'index']);
+Route::get('/movies/{id}', [MoviesController::class, 'single']);
+Route::get('/movies/search/{search}', [MoviesController::class, 'search']);
